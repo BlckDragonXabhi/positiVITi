ab038b3f72863b342c06bb7134117cafa7972a56
BlckDragonXabhi <abhilashsarangi222@gmail.com>
2023-06-03 00:26:29 +0530

    3rd commit

--- app/src/main/java/com/example/positiviti/MainActivity.kt
+++ app/src/main/java/com/example/positiviti/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.positiviti
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.demo)
+        setContentView(R.layout.login_activity)
+
+        val button = findViewById<Button>(R.id.loginButton)
+
+
+        button.setOnClickListener{
+
+            val intent= Intent(this,Navbaractivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
